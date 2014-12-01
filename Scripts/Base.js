8a569870fc2164f7642eaaae43f649d46ca582b6
Megalopsychia <rodrigovanoni@gmail.com>
2014-11-30 22:24:42 -0300

    Upgrade Nov 30th 2014
    Read changelog, fucker

--- Scripts/Base.js
+++ Scripts/Base.js
@@ -1,7 +1,6 @@
 ﻿
 var Money = 0;
 var Respect = 0;
-
 var MoneyRate = 1;
 
 var WeaponsInventory = [0, 0, 0];
@@ -9,42 +8,97 @@ var WeaponsName      = ['Knife', 'Gun', 'Uzi'];
 var WeaponsCost      = [20, 100, 200];
 var WeaponsBonus     = [0.5, 2, 5];
 var WeaponsCostRate = 1.15;
+var WeaponsCap = [5, 3, 2];
 
 var EmployeesInventory = [0, 0, 0];
-var EmployeesName = ['Carlos', 'Juan', 'Pepe'];
+var EmployeesName = ['Rookie', 'Veteran', 'Expert'];
 var EmployeesCost = [1000, 1500, 2500];
 var EmployeesStealRate = [0.5, 0.7, 1.0];
 var EmployeesRespectBonus = [10, 15, 30];
 var EmployeesLimitQuantity = [3, 3, 1];
 var EmployeesCostRate = 1.25;
 var EmployeesStealRateBonus = 0;
-var EmployeeCap = 5;
+var EmployeeCap = [4, 2, 2];
 
-var OrgInventory = [0, 0, 0, 0];
-var OrgNames = ['Base', 'Building', 'Car', 'Blinded Truck'];
-var OrgCost = [10000, 150000, 9500, 450000];
-var OrgBonus = ['Raises Employee Cap', 'Raises more the employee cap', 'Your employees steal faster', 'Your employees steal even faster']
-var OrgBonusRate = [5, 25, 0.5, 2]
+var OrgInventory = [0, 0, 0, 0, 0];
+var OrgNames = ['Arsenal', 'Base', 'Building', 'Car', 'Blinded Truck'];
+var OrgCost = [10000, 15000, 150000, 9500, 450000];
+var OrgBonus = ['Raises Weapons Cap', 'Raises Employee Cap', 'Raises more the employee cap', 'You gain access to a new and better shop', 'Your employees steal faster'];
+var OrgBonusRate = [3, 1, 2, 0, 2];
+var OrgBonusType = ['WeaponsCap', 'EmployeeCap', 'EmployeeCap', 'NewShop', 'EmployeesStealRate'];
+var OrgCap = [2, 2, 1, 1, 2];
 var OrgCostRate = 1.4;
 
 var OrgTabEnabled = false;
 var ShopTabEnabled = false;
 var EmployeeTabEnabled = false;
+var BaseTabEnabled = false;
 
-function OnLoad()
+var ExpireCookie = "; Expires = Thu, 31th Dec 2099 12:00:00 utc;";
+var DeleteCookie = "Expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+
+function SaveGame()
+{
+    document.cookie = "Money=" + Money + ExpireCookie;
+    document.cookie = "Respect=" + Respect + ExpireCookie;
+    document.cookie = "MoneyRate=" + MoneyRate + ExpireCookie;
+    document.cookie = "WeaponsInventory=" + WeaponsInventory + ExpireCookie;
+    document.cookie = "EmployeesInventory=" + EmployeesInventory + ExpireCookie;
+    document.cookie = "OrgInventory=" + OrgInventory + ExpireCookie;
+    document.cookie = "OrgTabEnabled=" + OrgTabEnabled + ExpireCookie;
+    document.cookie = "ShopTabEnabled=" + ShopTabEnabled + ExpireCookie;
+    document.cookie = "EmployeeTabEnabled=" + EmployeeTabEnabled + ExpireCookie;
+    document.cookie = "BaseTabEnabled=" + BaseTabEnabled + ExpireCookie;
+}
+function LoadGame()
+{
+    var cookie = document.cookie.split(";");
+
+    for (var i = 0; i < cookie.length; i++)
+    {
+        var subCookie = cookie[i].split("=");
+        alert(subCookie[1]);
+        switch(subCookie[0])
+        {
+            case ("Money"):
+                Money = subCookie[1];
+                break;
+            case ("Respect"):
+                Respect = subCookie[1];
+                break;
+        }
+    }
+
+    Refresh();
+}
+function DeleteGame()
+{
+    document.cookie = "Money=;" + DeleteCookie;
+    document.cookie = "Respect=;" + DeleteCookie;
+    document.cookie = "MoneyRate=;" + DeleteCookie;
+    document.cookie = "WeaponsInventory=;" + DeleteCookie;
+    document.cookie = "EmployeesInventory=;" + DeleteCookie;
+    document.cookie = "OrgInventory=;" + DeleteCookie;
+    document.cookie = "OrgTabEnabled=;" + DeleteCookie;
+    document.cookie = "ShopTabEnabled=;" + DeleteCookie;
+    document.cookie = "EmployeeTabEnabled=;" + DeleteCookie;
+    document.cookie = "BaseTabEnabled=;" + DeleteCookie;
+}
+function OnLoad() 
 {
     setInterval(Refresh, 500);
 
     HideTab('Shop');
     HideTab('Employees');
     HideTab('Organization');
+    HideTab('Base');
 
     SetMessage('MainMje', 'You are a low-level thief, cant really steal from others than homeless and old people.');
     SetMessage('MainSubMje', 'Do you really wanna steal from those poor people?');
 }
 function BuyWeapon(id)
 {
-    if (parseInt(Money) >= parseInt(WeaponsCost[id]))
+    if (parseInt(Money) >= parseInt(WeaponsCost[id]) && parseInt(WeaponsInventory[id]) < parseInt(WeaponsCap[id]))
     {
         Money = parseInt(Money) - parseInt(WeaponsCost[id]);
         WeaponsInventory[id]++;
@@ -67,7 +121,7 @@ function BuyWeapon(id)
 }
 function HireEmployee(id)
 {
-    if (Money >= EmployeesCost[id] && EmployeesInventory[id] < EmployeeCap)
+    if (Money >= EmployeesCost[id] && EmployeesInventory[id] < EmployeeCap[id])
     {
         Money -= EmployeesCost[id];
         EmployeesCost[id] *= EmployeesCostRate;
@@ -116,13 +170,13 @@ function RefreshWeaponsTable()
         var row = table.insertRow(table.rows.length);
 
         var cell = row.insertCell(0);
-        cell.innerText = WeaponsName[i];
+        cell.innerHTML = WeaponsName[i];
 
         var cell1 = row.insertCell(1);
-        cell1.innerText = WeaponsCost[i];
+        cell1.innerHTML = WeaponsCost[i];
 
         var cell2 = row.insertCell(2);
-        cell2.innerText = 'Steal Money +$' + WeaponsBonus[i];
+        cell2.innerHTML = 'Steal Money +$' + WeaponsBonus[i];
 
         var cell3 = row.insertCell(3);
         var btn = document.createElement('Button');
@@ -132,7 +186,7 @@ function RefreshWeaponsTable()
         cell3.appendChild(btn);
 
         var cell4 = row.insertCell(4);
-        cell4.innerText = WeaponsInventory[i];
+        cell4.innerHTML = WeaponsInventory[i] + '/' + WeaponsCap[i];
     }
 }
 function RefreshEmployeesTable()
@@ -141,7 +195,6 @@ function RefreshEmployeesTable()
 
     if (table.rows.length > 0) {
         var i = table.rows.length - 1;
-
         while (i > 0) {
             table.deleteRow(i);
             i--;
@@ -154,16 +207,16 @@ function RefreshEmployeesTable()
         var row = table.insertRow(table.rows.length);
 
         var cell = row.insertCell(0);
-        cell.innerText = EmployeesName[i];
+        cell.innerHTML = EmployeesName[i];
 
         var cell1 = row.insertCell(1);
-        cell1.innerText = EmployeesCost[i].toFixed(2);
+        cell1.innerHTML = EmployeesCost[i].toFixed(2);
 
         var cell2 = row.insertCell(2);
-        cell2.innerText = 'Steal Money *' + EmployeesStealRate[i].toFixed(3);
+        cell2.innerHTML = 'Steal Money *' + EmployeesStealRate[i].toFixed(2);
 
         var cell2 = row.insertCell(3);
-        cell2.innerText = '+' + EmployeesRespectBonus[i];
+        cell2.innerHTML = '+' + EmployeesRespectBonus[i];
 
         var cell4 = row.insertCell(4);
         var btn = document.createElement('Button');
@@ -173,10 +226,11 @@ function RefreshEmployeesTable()
         cell4.appendChild(btn);
 
         var cell5 = row.insertCell(5);
-        cell5.innerText = EmployeesInventory[i] + '/' + EmployeeCap;
+        cell5.innerHTML = EmployeesInventory[i] + '/' + EmployeeCap[i];
     }
 }
-function RefreshOrganizationTable() {
+function RefreshOrganizationTable()
+{
     var table = document.getElementById('OrganizationTable');
 
     if (table.rows.length > 0) {
@@ -193,13 +247,13 @@ function RefreshOrganizationTable() {
         var row = table.insertRow(table.rows.length);
 
         var cell = row.insertCell(0);
-        cell.innerText = OrgNames[i];
+        cell.innerHTML = OrgNames[i];
 
         var cell1 = row.insertCell(1);
-        cell1.innerText = OrgCost[i].toFixed(2);
+        cell1.innerHTML = OrgCost[i];
 
         var cell2 = row.insertCell(2);
-        cell2.innerText = OrgBonus[i] + ' ' + OrgBonusRate[i].toFixed(3);
+        cell2.innerHTML = OrgBonus[i] + ' +' + OrgBonusRate[i];
 
         var cell3 = row.insertCell(3);
         var btn = document.createElement('Button');
@@ -209,12 +263,48 @@ function RefreshOrganizationTable() {
         cell3.appendChild(btn);
 
         var cell4 = row.insertCell(4);
-        cell4.innerText = OrgInventory[i];
+        cell4.innerHTML = OrgInventory[i];
     }
 }
 function BuyOrgItem(id)
 {
+    if (parseInt(Money) >= parseInt(OrgCost[id]))
+    {
+        Money = parseInt(Money) - parseInt(OrgCost[id]);
+        OrgInventory[id]++;
 
+        switch(OrgBonusType)
+        {
+            case 'WeaponsCap':
+                for (var i = 0; i < WeaponsCap; i++){
+                    WeaponsCap[i] += OrgBonusRate[id];
+                }
+            break;
+            case 'EmployeeCap':
+                for (var i = 0; i < EmployeeCap; i++) {
+                    EmployeeCap[i] += OrgBonusRate[id];
+                }
+            break;
+            case 'NewShop':
+                //TODO NEW SHOP
+            break;
+            case 'EmployeesStealRate':
+                var cont = 0;
+                for (var i = 0; i < EmployeesInventory; i++) {
+                    cont += EmployeesInventory[i];
+                }
+
+            break;
+
+        }
+
+        if (OrgNames[id] == 'Base' && !BaseTabEnabled)
+        {
+            alert('Congratulations, you now have a brand new base!');
+            ShowTab('Base');
+        }
+        
+    }
 }
 function GetMoney()
 {
@@ -241,18 +331,18 @@ function Refresh()
     Money += (MoneyRate * EmployeesStealRateBonus) / 2;
 
     var div = document.getElementById('currentMoney');
-    div.innerText = 'Current Money: $' + Money.toFixed(2);
+    div.innerHTML = 'Current Money: $' + Money.toFixed(2);
 
     var div = document.getElementById('currentMoneyRate');
-    div.innerText = 'Money Steal Rate: +$' + MoneyRate.toFixed(2) + ' per steal.';
+    div.innerHTML = 'Money Steal Rate: +$' + MoneyRate.toFixed(2) + ' per steal.';
 
     if (EmployeeTabEnabled)
     {
         var div = document.getElementById('MoneyPerSec');
-        div.innerText = 'Money per second: +$' + (MoneyRate * EmployeesStealRateBonus).toFixed(3);
+        div.innerHTML = 'Money per second: +$' + (MoneyRate * EmployeesStealRateBonus).toFixed(3);
 
         var div = document.getElementById('currentRespect');
-        div.innerText = 'Respect: ' + Respect;
+        div.innerHTML = 'Respect: ' + Respect;
     }
 
     RefreshWeaponsTable();
@@ -262,7 +352,7 @@ function Refresh()
 function SetMessage(id, mje)
 {
     var mj1 = document.getElementById(id);
-    mj1.innerText = mje;
+    mj1.innerHTML = mje;
 }
 function HideTab(id)
 {
